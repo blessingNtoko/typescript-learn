f5b6573c2e1e1e7ca11110daa067494219d53466
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-23 11:43:29 +0200

    learning types

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+// Basic Types
+let id = 7;
+let company = "Umbral";
+let isPublished = true;
+let x = "Hello";
+let age;
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, 2, "3", 4];
+// Tuple
+let person = [1, "Umbral", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, "John"],
+    [2, "Jane"],
+    [3, "Han"],
+    [4, "Chewie"],
+];
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// Basic Types
+let id: number = 7;
+let company: string = "Umbral";
+let isPublished: boolean = true;
+let x: any = "Hello";
+let age: number;
+let ids: number[] = [1, 2, 3, 4, 5];
+let arr: any[] = [1, 2, "3", 4];
+
+// Tuple
+let person: [number, string, boolean] = [1, "Umbral", true];
+// Tuple Array
+let employee: [number, string][];
+employee = [
+    [1, "John"],
+    [2, "Jane"],
+    [3, "Han"],
+    [4, "Chewie"],
+]
