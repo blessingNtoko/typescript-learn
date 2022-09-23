2a5b437092091a5fec14d25bb8ed9cb6ede1a90b
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-23 12:42:50 +0200

    type assertion

--- dist/index.js
+++ dist/index.js
@@ -17,3 +17,30 @@ employee = [
     [3, "Han"],
     [4, "Chewie"],
 ];
+// Union
+let prodID = 22;
+prodID = "22";
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 1] = "Up";
+    Direction1[Direction1["Down"] = 2] = "Down";
+    Direction1[Direction1["Left"] = 3] = "Left";
+    Direction1[Direction1["Right"] = 4] = "Right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+const user = {
+    id: 1,
+    name: "John"
+};
+// Type Assertion
+let cid = "1";
+// let customerID = <number>cid
+let customerID = cid;
+console.log(customerID);
--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,44 @@ employee = [
     [2, "Jane"],
     [3, "Han"],
     [4, "Chewie"],
-]
+];
+
+// Union
+let prodID: string | number = 22;
+prodID = "22";
+
+// Enum
+enum Direction1 {
+    Up = 1,
+    Down,
+    Left,
+    Right
+}
+
+enum Direction2 {
+    Up = "Up",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right"
+}
+
+// Objects
+
+type User = {
+    id: number,
+    name: string
+}
+
+const user: User = {
+    id: 1,
+    name: "John"
+}
+
+// Type Assertion
+let cid: any = "1";
+// let customerID = <number>cid
+let customerID = cid as number;
+console.log(customerID);
+
+
+
